9bc8fbf4e8dfa1daaee3ab3a9b66eb21fad7314c
aSEK79 <n@asek.de>
2026-01-30 08:25:11 +0100

    Update and rename ZTI_Unna.ps1 to ZTI_UN.ps1

--- ZTI_UN.ps1
+++ ZTI_UN.ps1
@@ -1,9 +1,9 @@
 # Settings
-$OSName = 'Windows 11 24H2 x64'
+$OSName = 'Windows 11 25H2 x64'
 $OSEdition = 'Pro'
 $OSActivation = 'Retail'
 $OSLanguage = 'de-de'
-$GroupTag = 'AutopilotUnna'
+$GroupTag = 'AutopilotUN'
 $TimeZone = 'W. Europe Standard Time'
 $TimeServerUrl = "https://www.timeapi.io/api/time/current/zone?timeZone=Europe%2FBerlin"
 $OutputFile = "X:\AutopilotHash.csv"
